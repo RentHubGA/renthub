62af46420ba8f6bdcd99ce52c82f951fc306cc45
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2024-02-03 18:25:36 +1100

    Remove unnecessry code

--- main_app/forms.py
+++ main_app/forms.py
@@ -33,43 +33,7 @@ class RentingForm(forms.Form):
             Submit('submit', 'Submit', css_class="btn btn-primary"),
         )
     )
-
-# class CustomUserCreationForm(UserCreationForm):
-#     username = forms.CharField(label='username', min_length=5, max_length=150)  
-#     email = forms.EmailField(label='email')  
-#     password1 = forms.CharField(label='password', widget=forms.PasswordInput)  
-#     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)  
-
-#     def username_clean(self):  
-#         username = self.cleaned_data['username'].lower()  
-#         new = CustomUser.objects.filter(username = username)  
-#         if new.count():  
-#             raise ValidationError("User Already Exist")  
-#         return username
     
-#     def email_clean(self):  
-#         email = self.cleaned_data['email'].lower()  
-#         new = CustomUser.objects.filter(email=email)  
-#         if new.count():  
-#             raise ValidationError(" Email Already Exist")  
-#         return email 
-
-#     def clean_password2(self):  
-#         password1 = self.cleaned_data['password1']  
-#         password2 = self.cleaned_data['password2']  
-  
-#         if password1 and password2 and password1 != password2:  
-#             raise ValidationError("Password don't match")  
-#         return password2  
-  
-#     def save(self, commit = True):  
-#         user = CustomUser.objects.create_user(  
-#             self.cleaned_data['username'],  
-#             self.cleaned_data['email'],  
-#             self.cleaned_data['password1']  
-#         )  
-#         return user   
-
 User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
