55bc819e64482b5c3c8ff5bf3e4a4b835a39e9cf
Sierra Sutherland-Brown <115909601+sierrabardot@users.noreply.github.com>
2024-02-04 12:18:29 +1100

    Rent Product Function (#26)
    * Add models to admin
    
    * Add renting form to product details - form not complete yet
    
    * Install crispy forms and change renting form layout
    
    * Start rent_product function
    
    * Remove unnecessry code
    
    * Fix delete product bug with name not showing
    
    * Add booking function logic
    
    * Add error and success messages for reservations
    
    * Fix base.html indentation

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('products/', views.ProductList.as_view(), name='product_list'),
     path('products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('products/<int:pk>/rent_product/', views.rent_product, name='rent_product'),
     path('products/create/', views.ProductCreate.as_view(), name='product_create'),
     path('products/<int:pk>/update', views.ProductUpdate.as_view(), name='product_update'),
     path('products/<int:pk>/delete', views.ProductDelete.as_view(), name='product_delete'),
     path('reviewform/', views.reviewform, name='reviewform'),
     path('products/<int:pk>/add_image', views.add_image, name='add_image'),
-    path('products/<int:pk>/rent_product/', views.rent_product, name='rent_product'),
     ## accounts Root
     path('accounts/profile/<str:username>/', views.ProfileDetailView.as_view(), name='profile_detail'),
     path('accounts/profile/<str:username>/dashboard/', views.ProfileDashboard.as_view(), name='profile_dashboard' ),
