ec447ac12f2a4da7cda4092a2aac7dc3444fcb54
Cassio de Souza <cassio.candango@gmail.com>
2024-02-05 17:57:10 +0800

    fly-o settings

--- renthub/settings.py
+++ renthub/settings.py
@@ -14,24 +14,26 @@ from pathlib import Path
 
 import environ
 import os
+from django.core.management.utils import get_random_secret_key
 
-environ.Env()
-environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(f'{BASE_DIR}/.env')
 
+SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list, default=[])
+CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS', cast=list, default=[])
 
 # Application definition
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+
     # add django.contrib.gis
     # 'django.contrib.gis',
 ]
@@ -57,6 +60,7 @@ AUTH_USER_MODEL = 'main_app.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -65,12 +69,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# django-debug-toolbar
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    INTERNAL_IPS = ['127.0.0.1']
+
+
 ROOT_URLCONF = 'renthub.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join((BASE_DIR), 'templates/')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,20 +101,10 @@ WSGI_APPLICATION = 'renthub.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'renthub',
-    }
+    'default': env.db(default='postgresql://localhost/renthub')
 }
 
-STORAGES = {
-    'default': {
-        'BACKEND': 'storages.backends.s3.S3Storage'
-    },
-    'staticfiles': {
-      'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
-    }
-}
+
 
 
 # Password validation
@@ -141,11 +142,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = f'{BASE_DIR}/staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main_app/static')
-    ]
-
+# STORAGES = {
+#     'default': {
+#         'BACKEND': 'storages.backends.s3.S3Storage'
+#     },
+#     # 'staticfiles': {
+#     #   'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+#     # }
+# }
+DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
 # login redirect
 LOGIN_REDIRECT_URL = '/products/'
 # logout redirect
@@ -159,3 +167,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY_ID = env('AWS_SECRET_ACCESS_KEY_ID')
+S3_BUCKET = env('S3_BUCKET')
+S3_BASE_URL = env('S3_BASE_URL')
