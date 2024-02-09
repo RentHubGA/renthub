30927f2d6c9750c766cf9ec1f3256959976a2a5f
Sierra Sutherland-Brown <115909601+sierrabardot@users.noreply.github.com>
2024-02-09 18:21:15 +1100

    Add datepicker (#54)
    * Add models to admin
    
    * Add pagination
    
    * Add pagination to template
    
    * Add error handling if no products match queryset
    
    * Change pagination styling
    
    * Add dynamic datepicker with jQuery

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import get_unavailable_dates
 
 
 urlpatterns = [
@@ -14,6 +15,7 @@ urlpatterns = [
     path('products/<int:pk>/delete', views.ProductDelete.as_view(), name='product_delete'),
     path('products/<int:pk>/review', views.ReviewCreate.as_view(), name='review_create'),
     path('products/<int:pk>/add_image', views.add_image, name='add_image'),
+    path('get_unavailable_dates/<int:product_id>/', get_unavailable_dates, name='get_unavailable_dates'),
     ## accounts Root
     path('accounts/profile/<str:username>/detail', views.ProfileDetailView.as_view(), name='profile_detail'),
     path('accounts/profile/<str:username>/', views.Profile.as_view(), name='profile' ),
